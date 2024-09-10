3efd918437598d77cf6b2492b29c2b024eba44d8
stenliit <sten.liit@tktk.ee>
2024-09-10 16:02:29 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+//Rakendus küsib kasutajal tema nime mida peab ta sisestama
+// rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+// saad ka kasutada  Console.WriteLine("Hello" + ", " + userName);
+Console.WriteLine("Hello, " + userName + "!");  //Output
+
+//string interpolation parem varjant
+Console.WriteLine($"Hello, {userName} !"); //Output
+
